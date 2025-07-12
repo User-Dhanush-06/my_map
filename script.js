3f5278e70f17686f0b1a934fdefe788f6489d5b4
Dhanush Raman <vikiviky41@gmail.com>
2025-07-12 20:32:44 +0530

    Add map project
    Date : 12-07-2025
    Done : Just created a personal map for SAS ,need to execute all the other things

---
+++ script.js
@@ -0,0 +1,63 @@
+ mapboxgl.accessToken = 'pk.eyJ1IjoidXNlci1kaGFudXNoIiwiYSI6ImNtZDA3dm9zYzBwMXMybHFuMWdsd2t4MTYifQ.BoptndpbCXEVvWxHc1t1xg';
+
+ 
+   navigator.geolocation.getCurrentPosition(successLocation,
+    errorLocation, {
+        enableHighAccuracy:true
+    })
+
+    function successLocation(position){
+        console.log("Accuracy in meters:", position.coords.accuracy);
+        console.log("Lat:", position.coords.latitude, "Lon:", position.coords.longitude);
+        setupMap([position.coords.longitude, position.coords.latitude])
+    }
+
+    function errorLocation(){
+        setupMap([76.9589, 10.9974])
+    }
+
+    function setupMap(center) {
+        const map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: center,
+            zoom: 15
+        })
+
+        const nav = new mapboxgl.NavigationControl()
+        map.addControl(nav)
+
+        var directions = new MapboxDirections({
+        accessToken: mapboxgl.accessToken
+        });
+
+        map.addControl(directions, 'top-left');
+
+        const geolocate = new mapboxgl.GeolocateControl({
+            positionOptions: {
+                enableHighAccuracy: true
+            },
+            trackUserLocation: true,  // follows user as they move
+            showUserHeading: true,    // shows the direction the user is facing (if supported)
+            showAccuracyCircle: false
+        });
+        map.addControl(geolocate, 'top-left');
+
+        // Optional: trigger location when map is loaded
+        map.on('load', () => {
+            geolocate.trigger(); // automatically center on user's location
+
+            // Add marker manually on first geolocate event
+            map.on('geolocate', function(e) {
+                const lon = e.coords.longitude;
+                const lat = e.coords.latitude;
+
+                // Add a custom marker
+                new mapboxgl.Marker({ color: 'blue' })  // or customize icon
+                    .setLngLat([lon, lat])
+                    .setPopup(new mapboxgl.Popup().setText("You are here!"))
+                    .addTo(map);
+            });
+        });
+    }
+
